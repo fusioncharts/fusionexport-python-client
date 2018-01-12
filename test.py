58624118f5ddc02122546a29ec0aa24cec6711c2
Rousan Ali <ariyankhan46@gmail.com>
2018-01-12 23:25:37 +0530

    Added test cases

--- test.py
+++ test.py
@@ -23,14 +23,21 @@ def on_export_state_changed(event):
     print(event["state"])
 
 
-chart_config = read_file("./template-test/chart-config.json")
+chart_config = read_file("./template-test/dashboard/chart-config.json")
 export_server_host = "127.0.0.1"
 export_server_port = 1337
 
 export_config = ExportConfig()
 export_config["chartConfig"] = chart_config
-export_config["templateFilePath"] = "./template-test/html/template.html"
-export_config["resourceFilePath"] = "./template-test/resources.json"
+# export_config["exportAsZip"] = True
+# export_config["inputSVG"] = "./template-test/sample.svg"
+# export_config["type"] = "xsls"
+export_config["templateFilePath"] = "./template-test/dashboard/template.html"
+export_config["resourceFilePath"] = "./template-test/dashboard/resources.json"
+export_config["dashboardLogo"] = "/Users/rousan/Projects/fusionexport-python-client/template-test/dashboard/angular-smgkcu.zip"
+export_config["dashboardHeading"] = "AI is best"
+export_config["dashboardSubheading"] = "A sdf sfsd"
+# export_config["callbackFilePath"] = "./template-test/callback.js"
 
 em = ExportManager(export_server_host, export_server_port)
 em.export(export_config, on_export_done, on_export_state_changed)
