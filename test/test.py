0f8e19ed5c8b64f38bdc1694c46117ee6c0b7c71
Dhiresh <dhiresh.kumar@appwrk.com>
2021-02-06 12:21:46 +0530

    Revert "Test Case added for is_secure"
    This reverts commit c971ed7acb6a90e2479f88b86d2caae4c2caff28.

--- test/test.py
+++ test/test.py
@@ -1,33 +1,15 @@
 from fusionexport import ExportManager, ExportConfig
 
 export_config = ExportConfig()
-export_config["chartConfig"] = [{
-            "type": "column2d",
-            "renderAt": "chart-container",
-            "width": "600",
-            "height": "400",
-            "dataFormat": "json",
-            "dataSource": {
-                "chart": {
-                    "caption": "Number of visitors last week",
-                    "subCaption": "Bakersfield Central vs Los Angeles Topanga"
-                },
-                "data": [{
-                        "label": "Mon",
-                        "value": "15123"
-                    },{
-                        "label": "Tue",
-                        "value": "14233"
-                    },{
-                        "label": "Wed",
-                        "value": "25507"
-                    }
-                ]
-            }
-        }]
+export_config["chartConfig"] = "chart-config.json"
+# #export_config["inputSVG"] = "./chart.svg"
+# export_config["chartConfig"] = "./template-test/dashboard/chart-config.json"
+# export_config["templateFilePath"] = "./template-test/dashboard/template.html"
+# export_config["resourceFilePath"] = "./template-test/resources.json"
+
 export_server_host = "127.0.0.1"
 export_server_port = 1337
-is_secure = True
-em = ExportManager(export_server_host, export_server_port, is_secure)
+
+em = ExportManager(export_server_host, export_server_port)
 exported_files = em.export(export_config, "./exported", True)
 print(exported_files)
