03e5114b79414a81c2f8712c55cc0c3709479922
Sanjay Bhan <54075218+SanjayBhan@users.noreply.github.com>
2021-07-19 22:15:19 +0530

    Version change 2.0.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.rst') as readme_file:
 
 setup(
     name='fusionexport',
-    version='1.2.2',
+    version='2.0.0',
     description='The FusionExport SDK in Python',
     long_description=readme,
     author='FusionCharts',
